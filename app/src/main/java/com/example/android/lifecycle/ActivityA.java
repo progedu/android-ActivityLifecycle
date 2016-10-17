ac268a60c144509423072e971e6250ce05b9c43a
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-10-17 19:41:35 +0900

    Add onCreate

--- app/src/main/java/com/example/android/lifecycle/ActivityA.java
+++ app/src/main/java/com/example/android/lifecycle/ActivityA.java
@@ -2,12 +2,26 @@ package com.example.android.lifecycle;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.android.lifecycle.util.StatusTracker;
+import com.example.android.lifecycle.util.Utils;
 
 public class ActivityA extends Activity {
+
+    private String mActivityName;
+    private TextView mStatusView;
+    private TextView mStatusAllView;
+    private StatusTracker mStatusTracker = StatusTracker.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a);
+        mActivityName = getString(R.string.activity_a);
+        mStatusView = (TextView) findViewById(R.id.status_view_a);
+        mStatusAllView = (TextView) findViewById(R.id.status_view_all_a);
+        mStatusTracker.setStatus(mActivityName, getString(R.string.on_create));
+        Utils.printStatus(mStatusView, mStatusAllView);
     }
 }
